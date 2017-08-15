e89780e42c014b1bc37af2cbc6f36c954c97defe
sam <wade@heckledesign.com>
2017-08-15 14:40:11 -0600

    Working on Routes

--- src/app/app.module.js
+++ src/app/app.module.js
@@ -10,6 +10,8 @@ var platform_browser_1 = require("@angular/platform-browser");
 var forms_1 = require("@angular/forms");
 var app_component_1 = require("./app.component");
 var hero_detail_component_1 = require("./hero-detail.component");
+var heroes_component_1 = require("./heroes.component");
+var hero_service_1 = require("./hero.service");
 var AppModule = (function () {
     function AppModule() {
     }
@@ -23,7 +25,11 @@ AppModule = __decorate([
         ],
         declarations: [
             app_component_1.AppComponent,
-            hero_detail_component_1.HeroDetailComponent
+            hero_detail_component_1.HeroDetailComponent,
+            heroes_component_1.HeroesComponent
+        ],
+        providers: [
+            hero_service_1.HeroService
         ],
         bootstrap: [app_component_1.AppComponent]
     })
