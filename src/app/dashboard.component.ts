28551453ef59df0fceccf100d84fddb7e24147c6
sam <wade@heckledesign.com>
2017-08-15 15:24:59 -0600

    dashboard component changes - screen

--- src/app/dashboard.component.ts
+++ src/app/dashboard.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
 selector: 'my-dashboard',
-template: '<h3> My DashBoard </h3>'
+templateUrl: './dashboard.component.html'
 })
 
 export class DashBoardComponent {}
